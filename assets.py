6fc1ad809ce781f0e0616fd1d07c9839949dffc4
Henrique Puppi <kikepuppi@gmail.com>
2023-05-22 11:49:53 -0300

    outros botoes

--- assets.py
+++ assets.py
@@ -32,22 +32,22 @@ def load_assets():
     assets[TelaC] = pygame.transform.scale(assets[TelaC], (largura, altura))
     btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
     btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
-    btns[Comprar] = [pygame.image.load(os.path.join(Botoes, 'Comprar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Comprar2.png')).convert()]
-    btns[Comprar] = pygame.transform.scale(assets[Comprar], (largura, altura))
+    btns[Voltar] = [pygame.image.load(os.path.join(Botoes, 'Voltar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Voltar2.png')).convert()]
+    btns[Voltar] = pygame.transform.scale(assets[Voltar], (largura, altura))
+    btns[Interrogacao] = [pygame.image.load(os.path.join(Botoes, 'Interrogacao1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Interrogacao2.png')).convert()]
+    btns[Interrogacao] = pygame.transform.scale(assets[Interrogacao], (largura, altura))
+    btns[LoadGame] = [pygame.image.load(os.path.join(Botoes, 'LoadGame1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'LoadGame2.png')).convert()]
+    btns[LoadGame] = pygame.transform.scale(assets[LoadGame], (largura, altura))
+    btns[NewGame] = [pygame.image.load(os.path.join(Botoes, 'NewGame1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'NewGame2.png')).convert()]
+    btns[NewGame] = pygame.transform.scale(assets[NewGame], (largura, altura))
+    btns[Selecionado] = [pygame.image.load(os.path.join(Botoes, 'Selecionado1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Selecionado2.png')).convert()]
+    btns[Selecionado] = pygame.transform.scale(assets[Selecionado], (largura, altura))
+    btns[Selecionar] = [pygame.image.load(os.path.join(Botoes, 'Selecionar1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Selecionar2.png')).convert()]
+    btns[Selecionar] = pygame.transform.scale(assets[Selecionar], (largura, altura))
+    btns[BSkins] = [pygame.image.load(os.path.join(Botoes, 'Skins1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Skins2.png')).convert()]
+    btns[BSkins] = pygame.transform.scale(assets[BSkins], (largura, altura))
+    btns[Upgrade] = [pygame.image.load(os.path.join(Botoes, 'Upgrade1.png')).convert(),pygame.image.load(os.path.join(Botoes, 'Upgrade2.png')).convert()]
+    btns[Upgrade] = pygame.transform.scale(assets[Upgrade], (largura, altura))
 
 
     return [assets, btns]
