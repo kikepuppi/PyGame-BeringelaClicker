87a266e9a317cab888adc3ce2d79068a2ee768eb
Henrique Puppi <kikepuppi@gmail.com>
2023-05-27 15:13:10 -0300

    missoes logica

--- missoes.py
+++ missoes.py
@@ -17,11 +17,12 @@ Up3 = goods['Up3']
 Up4 = goods['Up4']
 Up5 = goods['Up5']
 Up6 = goods['Up6']
-clicks = 'x'
-auto = goods['Auto']
-acumulado = 'x'
+clicks = 99
+#auto = goods['Auto']
+auto = 1
+acumulado = 1
 
-def missoes():
+def listamissoes():
     missoes = {
     1:['Clique na Berinjela 100 vezes', clicks, 100],
     2:['Tenha 5 Clicks', Up1, 5],
@@ -45,5 +46,14 @@ def missoes():
     20:['Tenha 10 Super Clicks', Up5, 10]
     }
     return missoes
-    
 
+i = 1
+
+missoes = listamissoes()
+missao_atual = missoes[i]
+nome_missao = missao_atual[0]
+check = missao_atual[1]
+complete = missao_atual[2]
+print('{0}/{1}'.format(check, complete))
+if check >= complete:
+      i+=1
